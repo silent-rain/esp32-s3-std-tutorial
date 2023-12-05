fef6846d90671c58d22ad15bf0bf01a081863999
silent-rain <2367221387@qq.com>
2023-12-05 23:51:58 +0800

    Hello World

---
+++ src/bin/basic/hello_world.rs
@@ -0,0 +1,15 @@
+//! # Hello World
+//! ## 运行
+//! ```shell
+//! cargo run --bin hello_world
+//! ```
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    log::info!("Hello, world!");
+}
