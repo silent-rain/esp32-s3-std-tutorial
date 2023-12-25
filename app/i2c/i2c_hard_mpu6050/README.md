8edb6d91ddff8a304ff38ed1f8ccdde4308f44f6
silent-rain <2367221387@qq.com>
2023-12-25 22:09:06 +0800

    硬件读写 MPU6050示例

---
+++ app/i2c/i2c_hard_mpu6050/README.md
@@ -0,0 +1,16 @@
+# 硬件读写 MPU6050
+
+## 引脚
+
+### MPU6050
+
+- 正极: VCC
+- 负极: GND
+- SDA: IO5
+- SCL: IO6
+
+## 执行指令
+
+```shell
+cargo run -r -p i2c_hard_mpu6050
+```
---
+++ app/i2c/i2c_hard_mpu6050/src/main.rs
@@ -0,0 +1,37 @@
+use mpu6050::hal::Mpu6050;
+
+use esp_idf_svc::{
+    hal::{delay::FreeRtos, peripherals::Peripherals},
+    log::EspLogger,
+    sys::link_patches,
+};
+
+fn main() -> anyhow::Result<()> {
+    link_patches();
+    // Bind the log crate to the ESP Logging facilities
+    EspLogger::initialize_default();
+    // 设置日志级别
+    log::set_max_level(log::LevelFilter::Info);
+
+    // Get the peripherals
+    let peripherals = Peripherals::take()?;
+
+    let i2c = peripherals.i2c0;
+    let sda = peripherals.pins.gpio5;
+    let scl = peripherals.pins.gpio6;
+
+    let mut mpu = Mpu6050::new(i2c, sda, scl)?;
+
+    let id = mpu.get_id()?;
+    log::info!("MPU6050 ID {id}");
+
+    log::info!("loop");
+    loop {
+        let data = mpu.get_data()?;
+
+        // 打印读取到的数据
+        println!("Accel: ({}, {}, {})", data.acc_x, data.acc_y, data.acc_z);
+        println!("Gyro: ({}, {}, {})", data.gyro_x, data.gyro_y, data.gyro_z);
+        FreeRtos::delay_ms(1000);
+    }
+}
