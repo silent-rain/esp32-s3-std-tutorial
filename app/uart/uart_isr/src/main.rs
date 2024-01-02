64d41d359d6a14c9bf116a502d8177a676e6b681
silent-rain <2367221387@qq.com>
2024-01-02 23:34:35 +0800

    串口中断示例调试

--- app/uart/uart_isr/src/main.rs
+++ app/uart/uart_isr/src/main.rs
@@ -1,7 +1,10 @@
+use std::ffi::c_int;
+
 use esp_idf_svc::{
     hal::{
         delay::BLOCK,
         gpio,
+        interrupt::InterruptType,
         peripherals::Peripherals,
         uart::{
             config::{DataBits, StopBits},
@@ -10,8 +13,15 @@ use esp_idf_svc::{
         units::Hertz,
     },
     log::EspLogger,
-    sys::{gpio_install_isr_service, gpio_isr_handler_add, link_patches, ESP_INTR_FLAG_LEVEL1},
+    sys::{
+        link_patches, uart_driver_install, uart_enable_rx_intr, uart_isr_handle_t, uart_port_t,
+        QueueHandle_t, UART_NUM_1,
+    },
 };
+
+const UART_NUM: uart_port_t = UART_NUM_1 as i32; // 使用 UART0 端口
+const BUF_SIZE: c_int = 1024; // 设置接收缓冲区的大小为 1024 字节
+const UART_QUEUE_SIZE: c_int = 10; // 设置接收队列的大小为 10
 
 fn main() -> anyhow::Result<()> {
     link_patches();
@@ -26,11 +36,13 @@ fn main() -> anyhow::Result<()> {
     let tx = peripherals.pins.gpio12;
     let rx = peripherals.pins.gpio13;
 
-    let config = UartConfig::new()
+    let mut config = UartConfig::new()
         .baudrate(Hertz(115_200))
         .stop_bits(StopBits::STOP2)
         .data_bits(DataBits::DataBits8)
         .parity_none();
+    config.intr_flags = InterruptType::Iram.into();
+
     let mut uart = UartDriver::new(
         peripherals.uart1,
         tx,
@@ -41,9 +53,17 @@ fn main() -> anyhow::Result<()> {
     )?;
 
     unsafe {
-        gpio_install_isr_service(ESP_INTR_FLAG_LEVEL1 as i32);
+        // 用来保存接收队列的句柄
+        let uart_queue: *mut QueueHandle_t = todo!();
 
-        gpio_isr_handler_add(13, Some(gpio_isr_handler), std::ptr::null_mut());
+        uart_driver_install(UART_NUM, BUF_SIZE, BUF_SIZE, UART_QUEUE_SIZE, uart_queue, 0);
+
+        // 设置uart中断回调函数
+        // uart_isr_register(UART_NUM, uart_isr, NULL, ESP_INTR_FLAG_IRAM, NULL);
+        //
+
+        // 使能uart接收中断
+        uart_enable_rx_intr(UART_NUM);
     }
 
     loop {
